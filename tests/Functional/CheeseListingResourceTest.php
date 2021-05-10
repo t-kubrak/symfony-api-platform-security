213b4901ba9d86eab9a9ea9ea3755ad1025b01aa
tkubrak <taras.kubrak@dataart.com>
2021-05-10 14:18:27 +0300

    feat: initial api test

---
+++ tests/Functional/CheeseListingResourceTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Tests\Functional;
+
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+
+class CheeseListingResourceTest extends ApiTestCase
+{
+    public function testCreateCheeseListing()
+    {
+        $client = self::createClient();
+
+        $client->request('POST', '/api/cheeses', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [],
+        ]);
+
+        $this->assertResponseStatusCodeSame(401);
+    }
+}
