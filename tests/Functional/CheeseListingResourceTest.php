28c2c712de02eb830cab128e30ee52b6e4d96658
tkubrak <taras.kubrak@dataart.com>
2021-05-10 14:23:46 +0300

    feat: logging in the user inside the test

--- tests/Functional/CheeseListingResourceTest.php
+++ tests/Functional/CheeseListingResourceTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Functional;
 
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
 
 class CheeseListingResourceTest extends ApiTestCase
 {
@@ -18,5 +19,24 @@ class CheeseListingResourceTest extends ApiTestCase
         ]);
 
         $this->assertResponseStatusCodeSame(401);
+
+        $user = new User();
+        $user->setEmail('cheeseplease@example.com');
+        $user->setUsername('cheeseplease');
+        $user->setPassword('$argon2id$v=19$m=65536,t=6,p=1$AIC3IESQ64NgHfpVQZqviw$1c7M56xyiaQFBjlUBc7T0s53/PzZCjV56lbHnhOUXx8');
+
+        $em = self::$container->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $client->request('POST', '/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => 'cheeseplease@example.com',
+                'password' => 'foo'
+            ],
+        ]);
+
+        $this->assertResponseStatusCodeSame(204);
     }
 }
