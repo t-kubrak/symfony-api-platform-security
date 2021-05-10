ad6f526fd003d0753d2e03163cdcd7bcf036f7ea
tkubrak <taras.kubrak@dataart.com>
2021-05-10 12:13:56 +0300

    feat: logout and passing api data to js on page load

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,4 +24,12 @@ class SecurityController extends AbstractController
             'Location' => $iriConverter->getIriFromItem($this->getUser())
         ]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('should not be reached');
+    }
 }
